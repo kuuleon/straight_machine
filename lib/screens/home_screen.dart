ced2a67808ac1d5b7d6b037af019dc11c7f053b8
Shintaro Miyazaki <tudopassa427911@gmail.com>
2022-10-25 17:21:18 +0900

    soundPath ver1

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -13,6 +14,9 @@ class _HomeScreenState extends State<HomeScreen> {
   // String _text05 = "不合格です";
   // String _text06 = "がんばりましょう";
 
+  // AudioPlayerクラスのインスタンス宣言
+  late AudioPlayer _audioPlayer;
+
   List<String> _texts = [
     "おめでとうございます",
     "合格です",
@@ -21,6 +25,29 @@ class _HomeScreenState extends State<HomeScreen> {
     "不合格です",
     "がんばりましょう"
   ];
+
+  List<String> _soundPaths = [
+    "assets/sounds/sound1.mp3",
+    "assets/sounds/sound2.mp3",
+    "assets/sounds/sound3.mp3",
+    "assets/sounds/sound4.mp3",
+    "assets/sounds/sound5.mp3",
+    "assets/sounds/sound6.mp3"
+  ];
+
+  // AudioPlayerクラスのインスタンス生成
+  @override
+  void initState() {
+    super.initState();
+    _audioPlayer = AudioPlayer();
+  }
+
+  // AudioPlayerクラスのインスタンス破棄
+  @override
+  void dispose() {
+    _audioPlayer.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,8 +65,14 @@ class _HomeScreenState extends State<HomeScreen> {
               child: Row(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  Expanded(flex: 1, child: _soundButton(_texts[0])),
-                  Expanded(flex: 1, child: _soundButton(_texts[1]))
+                  Expanded(
+                    flex: 1,
+                    child: _soundButton(_texts[0], _soundPaths[0]),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: _soundButton(_texts[1], _soundPaths[1]),
+                  )
                 ],
               ),
             ),
@@ -48,8 +81,14 @@ class _HomeScreenState extends State<HomeScreen> {
               child: Row(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  Expanded(flex: 1, child: _soundButton(_texts[2])),
-                  Expanded(flex: 1, child: _soundButton(_texts[3]))
+                  Expanded(
+                    flex: 1,
+                    child: _soundButton(_texts[2], _soundPaths[2]),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: _soundButton(_texts[3], _soundPaths[3]),
+                  )
                 ],
               ),
             ),
@@ -58,8 +97,14 @@ class _HomeScreenState extends State<HomeScreen> {
               child: Row(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  Expanded(flex: 1, child: _soundButton(_texts[4])),
-                  Expanded(flex: 1, child: _soundButton(_texts[5]))
+                  Expanded(
+                    flex: 1,
+                    child: _soundButton(_texts[4], _soundPaths[4]),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: _soundButton(_texts[5], _soundPaths[5]),
+                  )
                 ],
               ),
             )
@@ -69,9 +114,17 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  Widget _soundButton(String displayText) {
+  Widget _soundButton(String displayText, String soundPath) {
     return Container(
         padding: EdgeInsets.all(8.0),
-        child: ElevatedButton(onPressed: null, child: Text(displayText)));
+        child: ElevatedButton(
+            onPressed: (() => _playSound(soundPath)),
+            child: Text(displayText)));
+  }
+
+  //音を鳴らす段階で音ファイルをセット、同時に鳴らせる音は一つだけパターン
+  _playSound(String soundPath) async {
+    await _audioPlayer.setAsset(soundPath);
+    _audioPlayer.play();
   }
 }
